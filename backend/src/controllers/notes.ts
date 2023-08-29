ddbffd0ca01e6bfdc3ac48a58e397caf1dadfcf9
DeepakBarwal <deepakbarwal98@gmail.com>
2023-08-29 17:46:21 +0530

    Improved express http error handling

--- backend/src/controllers/notes.ts
+++ backend/src/controllers/notes.ts
@@ -1,5 +1,7 @@
 import { RequestHandler } from "express";
 import NoteModel from '../models/note';
+import createHttpError from "http-errors";
+import mongoose from "mongoose";
 
 export const getAllNotes: RequestHandler = async (req, res, next) => {
     try {
@@ -13,17 +15,35 @@ export const getAllNotes: RequestHandler = async (req, res, next) => {
 export const getNote: RequestHandler = async (req, res, next) => {
     const { noteId } = req.params;
     try {
+        if (!mongoose.isValidObjectId(noteId)) {
+            throw createHttpError(400, "Invalid note id");
+        }
+
         const note = await NoteModel.findById(noteId).exec();
+
+        if (!note) {
+            throw createHttpError(404, 'Note not found');
+        }
+
         res.status(200).json(note);
     } catch (error) {
         next(error);
     }
 }
 
-export const createNote: RequestHandler = async (req, res, next) => {
+interface ICreateNoteBody {
+    title?: string;
+    text?: string;
+}
+
+export const createNote: RequestHandler<unknown, unknown, ICreateNoteBody, unknown> = async (req, res, next) => {
     const { title, text } = req.body;
 
     try {
+        if (!title) {
+            throw createHttpError(400, 'Note must have a title');
+        }
+
         const newNote = await NoteModel.create({
             title,
             text
