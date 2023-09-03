1b21100d29162cec45d7824ff9b8992b2e7c7456
DeepakBarwal <deepakbarwal98@gmail.com>
2023-09-03 12:05:02 +0530

    Delete + Update notes from frontend

--- frontend/src/network/notes_api.ts
+++ frontend/src/network/notes_api.ts
@@ -33,3 +33,20 @@ export async function createNote(note: NoteInput): Promise<Note> {
     });
     return response.json();
 }
+
+export async function deleteNote(noteId: string) {
+    await fetchData(`/notes/${noteId}`, {
+        method: 'DELETE',
+    });
+}
+
+export async function updateNote(noteId: string, note: NoteInput): Promise<Note> {
+    const response = await fetchData(`/notes/${noteId}`, {
+        method: 'PATCH',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(note)
+    });
+    return response.json();
+}
